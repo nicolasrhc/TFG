93b3791e140336013cacb0bed1e83a518aa21f8d
Nicoflash <nicoflash@MacBook-Pro-de-Nicoflash.local>
2020-05-02 17:44:44 +0200

    - añadido archivo de prueba

---
+++ src/views/PruebaView.tsx
@@ -0,0 +1,12 @@
+import { Component, ReactNode } from "react"
+import React from "react"
+
+class PruebaView extends Component {
+    render(): ReactNode{
+        return(
+        <div>hola</div>
+        )
+    }
+}
+
+export default PruebaView;
